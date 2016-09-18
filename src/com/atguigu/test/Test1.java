caf1ed91925aa0a2f2682e6fff62eed03f36e339
xuesong <lxs866@126.com>
2016-09-18 18:37:18 +0800

    Update Test1.java

--- src/com/atguigu/test/Test1.java
+++ src/com/atguigu/test/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("add000000000000000001");
 		System.out.println("add044");
+		System.out.println("服务器");
 	}
 
 }
